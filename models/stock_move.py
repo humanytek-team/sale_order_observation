483fbc83d29448b12ed4bd3a8feb4c2d4bd9d04c
Ruben Dario Bravo <rbravo@hoatzin.org>
2017-12-21 13:59:57 -0400

    observaciones en el albaran de salida

---
+++ models/stock_move.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2017 Humanytek (<www.humanytek.com>).
+#    Rubén Bravo <rubenred18@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class StockMove(models.Model):
+    _name = "stock.move"
+    _inherit = 'stock.move'
+
+    sale_line_observation = fields.Text('Observation',
+                            compute='_compute_observation', readonly=True)
+
+    @api.one
+    def _compute_observation(self):
+        moves = self.search([('move_dest_id.id', '=', self.id)])
+        if moves:
+            return moves[0].production_id.sale_line_observation
